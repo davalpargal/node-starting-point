5ab2a03d3cf9b2d5e3b6a36b19159fa1a548ce6e
admin <admin@Abhinav-Rai.local>
2017-05-23 16:42:08 +0530

    Math functions added

--- src/calculator.js
+++ src/calculator.js
@@ -8,7 +8,7 @@ function subtract(a,b){
 
 function divide(a,b){
 	var div=a/b;
-    if(isNaN(div)) return "NaN";
+    if(isNaN(div)) throw new Error('Not a number error');
 	else if(!isFinite(div)) return "Infinity";
 	else return div;
 }
@@ -21,14 +21,44 @@ function abs(a){
   return Math.abs(a);
 }
 
+function sqr(a){
+  return a*a;
+}
 
+function sqrRoot(a){
+  if(a<0) throw new Error("Get a life bro!!")
+  return Math.sqrt(a);
+}
+
+function neg(a)
+{
+  return -a;
+}
+
+function croot(a){
+  return Math.cbrt(a);
+}
+
+function cube(a){
+  return a*a*a;
+}
+
+function sin(a){
+  return Math.sin(a);
+}
 
 module.exports = {
   sum: sum,
   subtract: subtract,
   divide: divide,
   multiply: multiply,
-  absolute: abs
+  absolute: abs,
+  sqr: sqr,
+  sqrRoot:sqrRoot,
+  negation: neg,
+  croot: croot,
+  cube: cube,
+  sin: sin
 }
 
 
--- test/calculator-test.js
+++ test/calculator-test.js
@@ -24,12 +24,12 @@ describe('Calculator', function() {
   	 });
 
   	 it('Infinity when inputs are 3,0',function(){
-  	 	assert.equal("Infinity",Calculator.divide(3,0))
+  	 	assert.equal("Infinity",Calculator.divide(3,0));
   	 });
 
-  	 it('NaN when inputs are 0,0',function(){
-  	 	assert.equal("NaN",Calculator.divide(0,0))
-  	 });
+  	 it('should return Error when Not a number', function() {
+      assert.throws(function(){return Calculator.divide(0,0)}, Error, 'Calculator.divide() did not work in test');
+    });
   });
   
   describe('multiply', function(){
@@ -46,5 +46,59 @@ describe('Calculator', function() {
       assert.equal(3,Calculator.absolute(3));
     });
   });
+
+  describe('sqr',function(){
+    it('should return 4 when input is 2',function(){
+      assert.equal(4,Calculator.sqr(2));
+    });
+    it('should return 4 when input is -2',function(){
+      assert.equal(4,Calculator.sqr(2));
+    });
+  });
+  
+  describe('sqrRoot',function(){
+    it('should return 4 when input is 16',function(){
+      assert.equal(4,Calculator.sqrRoot(16));
+    });
+    it('should return Error when input is -16',function(){
+      assert.throws(function(){return Calculator.sqrRoot(-16),Error,'Function dod not work'});
+    });
+  });
+  
+  describe('Negation',function(){
+    it('should return 3 when input is -3',function(){
+      assert.equal(3,Calculator.negation(-3));
+    });
+    it('should return -3 when input is 3',function(){
+      assert.equal(-3,Calculator.negation(3));
+    });
+  });
+
+  describe('Cuberoot',function(){
+    it('should return -3 when input is -27',function(){
+      assert.equal(-3,Calculator.croot(-27));
+    });
+  });
+
+  describe('Cube',function(){
+    it('should return 27 when input is 3',function(){
+      assert.equal(27,Calculator.cube(3));
+    });
+  });
+  
+  describe('sin',function(){
+    it('should return 1 when input is Pi',function(){
+      assert.equal(1.2246467991473532e-16,Calculator.sin(Math.PI));
+    });
+    it('should return -1 when input is -Pi',function(){
+      assert.equal(-1.2246467991473532e-16,Calculator.sin(-Math.PI));
+    });
+    it('should return 0 when input is 0',function(){
+      assert.equal(0,Calculator.sin(0));
+    });
+
+  });
+
+
 
 });
