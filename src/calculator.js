5d6261694e5afc8412e3bcf42e986a689bc82794
admin <admin@Abhinav-Rai.local>
2017-05-23 15:24:09 +0530

    abs added

--- src/calculator.js
+++ src/calculator.js
@@ -1,23 +1,34 @@
 function sum(a, b) {
   return a+b;
 }
+
 function subtract(a,b){
 	return a-b;
 }
+
 function divide(a,b){
 	var div=a/b;
     if(isNaN(div)) return "NaN";
 	else if(!isFinite(div)) return "Infinity";
 	else return div;
 }
+
 function multiply(a,b){
 	return a*b;
 }
+
+function abs(a){
+  return Math.abs(a);
+}
+
+
+
 module.exports = {
   sum: sum,
   subtract: subtract,
   divide: divide,
-  multiply: multiply
+  multiply: multiply,
+  absolute: abs
 }
 
 
--- test/calculator-test.js
+++ test/calculator-test.js
@@ -5,17 +5,18 @@ var Calculator = require('../src/calculator');
 describe('Calculator', function() {
 
   describe('sum', function() {
-
     it('should return 1 when inputs are 1,0', function(){
       assert.equal(1, Calculator.sum(1,0))	
     });
 
   });  
+  
   describe('subtract', function(){
      it('should return 0 when inputs are 1,1',function(){
        assert.equal(0,Calculator.subtract(1,1))
      });
   });
+  
   describe('divide',function(){
 
   	 it('should return 4 when inputs are 8,2',function(){
@@ -30,9 +31,20 @@ describe('Calculator', function() {
   	 	assert.equal("NaN",Calculator.divide(0,0))
   	 });
   });
+  
   describe('multiply', function(){
      it('should return 10 when inputs are 5,2',function(){
        assert.equal(10,Calculator.multiply(5,2))
      });
   });
+
+  describe('absolute',function(){
+    it('should return 3 when input is -3',function(){
+      assert.equal(3,Calculator.absolute(-3));
+    });
+    it('should return 3 when input is 3',function(){
+      assert.equal(3,Calculator.absolute(3));
+    });
+  });
+
 });
